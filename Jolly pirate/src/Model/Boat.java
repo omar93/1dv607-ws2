7e7dbb50995d086eff69ed2229f9117847b65fd2
Omar Alhamad <omar93@hotmail.se>
2021-09-23 01:37:44 +0200

    updated boatclass with enum and some getters

--- Jolly pirate/src/Model/Boat.java
+++ Jolly pirate/src/Model/Boat.java
@@ -1,11 +1,28 @@
 package Model;
 
 public class Boat {
+
+    // Members
     private String boatID = "0";
+    private double length = 0;
+    private Type type;
 
-    public Boat(){
-
+    // Enum for boat types
+    enum Type {
+        SAILBOAT, 
+        MOTORSAILER, 
+        KAYAK,
+        CANOE, 
+        OTHER
     }
 
+    // Constructor
+    public Boat(Type type){
+        this.type = type;
+    }
+
+    // Getters and setters for the boat class
     public String getId() { return this.boatID; }
+    public Type getType() { return this.type; }
+    public double getLength() { return this.length; }
 }
